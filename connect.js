d6938b0e72fb0927e108d035a4db7bc7d49c5a00
Boris Fritscher <boris@fritscher.ch>
2019-07-25 14:32:10 +0200

    init if content already loaded

--- connect.js
+++ connect.js
@@ -19,58 +19,56 @@ Create iframe button and listen to events
     }
   });
 
-  window.addEventListener(
-    "DOMContentLoaded",
-    () => {
-      const iframe = document.createElement("iframe");
+  function init() {
+    const iframe = document.createElement("iframe");
 
-      if (!baseURL) throw new Error("baseURL not found");
+    if (!baseURL) throw new Error("baseURL not found");
 
-      iframe.setAttribute("src", baseURL + "bridge.html");
+    iframe.setAttribute("src", baseURL + "bridge.html");
 
-      window.addEventListener(
-        "error",
-        e => {
-          iframe.contentWindow.postMessage(
-            { type: "scriptError", msg: e.message },
-            "*"
-          );
-        },
-        false
-      );
-
-      window.highlightBlock = function highlightBlock(id) {
+    window.addEventListener(
+      "error",
+      e => {
         iframe.contentWindow.postMessage(
-          { type: "highlightBlock", id: id },
+          { type: "scriptError", msg: e.message },
           "*"
         );
-      };
+      },
+      false
+    );
 
-      window.addEventListener("message", event => {
-        if (event.source !== iframe.contentWindow) return;
-        const msg = event.data;
-        if (msg.type === "init") {
-          iframe.setAttribute("style", msg.style);
-          const response = {
-            type: "initResponse",
-            blocklySource: window.BLOCKLY_SOURCE || null,
-            location: window.location.href
-          };
-          iframe.contentWindow.postMessage(response, "*");
-        } else if (msg.type == "reload") {
-          sessionStorage.USE_BLOCKLY_CODE = "INDEED";
-          window.location.reload();
-        } else if (msg.type == "script") {
-          const script = document.createElement("script");
-          script.appendChild(document.createTextNode(msg.script));
-          document.body.appendChild(script);
-          iframe.contentWindow.postMessage({ type: "scriptInjected" }, "*");
-        }
-      });
+    window.highlightBlock = function highlightBlock(id) {
+      iframe.contentWindow.postMessage({ type: "highlightBlock", id: id }, "*");
+    };
 
-      iframe.setAttribute("style", DEFAULT_STYLE);
-      document.body.appendChild(iframe);
-    },
-    false
-  );
+    window.addEventListener("message", event => {
+      if (event.source !== iframe.contentWindow) return;
+      const msg = event.data;
+      if (msg.type === "init") {
+        iframe.setAttribute("style", msg.style);
+        const response = {
+          type: "initResponse",
+          blocklySource: window.BLOCKLY_SOURCE || null,
+          location: window.location.href
+        };
+        iframe.contentWindow.postMessage(response, "*");
+      } else if (msg.type == "reload") {
+        sessionStorage.USE_BLOCKLY_CODE = "INDEED";
+        window.location.reload();
+      } else if (msg.type == "script") {
+        const script = document.createElement("script");
+        script.appendChild(document.createTextNode(msg.script));
+        document.body.appendChild(script);
+        iframe.contentWindow.postMessage({ type: "scriptInjected" }, "*");
+      }
+    });
+
+    iframe.setAttribute("style", DEFAULT_STYLE);
+    document.body.appendChild(iframe);
+  }
+
+  if (document.readyState === "complete" || document.readyState === "loaded") {
+    init();
+  }
+  window.addEventListener("DOMContentLoaded", init, false);
 })();
